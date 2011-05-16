407fa206651233c15e246f0ade2c8f1884a27b32
Hajime Nakagami <nakagami@gmail.com>
2011-05-16 17:52:35 +0900

    update_rss() logic move to models.py

--- planet/management/commands/syncrss.py
+++ planet/management/commands/syncrss.py
@@ -21,55 +21,8 @@
 import sys, urllib2, datetime
 import feedparser
 from django.core.management.base import BaseCommand
-from planet.models import Feed, Entry
+from planet.models import update_rss, Feed, Entry
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        NOW = datetime.datetime.utcnow()
-        proxy = kwargs.get('proxy')
-        if proxy:
-            proxy_handlers = [urllib2.ProxyHandler({"http": proxy})]
-        else:
-            proxy_handlers = None
-        
-        for f in Feed.objects.all():
-            # Sync RSS feed.
-            if proxy_handlers:
-                d = feedparser.parse(f.rss_url, handlers = proxy_handlers)
-            else:
-                d = feedparser.parse(f.rss_url)
-        
-            if d['feed'].get('title'):
-                f.title = d.feed.title
-            if d['feed'].get('link'):
-                f.link = d.feed.link
-            if d['feed'].get('subtitle'):
-                f.subtitle = d.feed.subtitle
-            if d['feed'].get('author'):
-                f.author = d.feed.author
-            f.save()
-        
-            # Sync feed entries.
-            for e in d.entries:
-                t = e.get('updated_parsed') 
-                if t:
-                    t = datetime.datetime(*t[:7])
-                    if t > NOW:
-                        continue
-                o = Entry.objects.filter(link=e.link)
-                if o:
-                    o = o[0]                # Find a entry (probably 1 entry)
-                else:
-                    o = Entry(feed_id=f.id) # Add new entry
-                    o.link = e.get('link')
-                o.title = e.get('title')
-                o.description = e.get('description', '')
-                o.author = e.get('author', '')
-                if t: 
-                    o.pub_dttm = t
-                    if f.pub_dttm_offset:
-                        o.pub_dttm += datetime.timedelta(0, f.pub_dttm_offset * 60)
-                elif not o.pub_dttm:
-                    o.pub_dttm = NOW
-                o.save()
-
+        update_rss(proxy=kwargs.get('proxy'))
