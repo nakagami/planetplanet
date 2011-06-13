c2148166b41260ca00c5a917d9923bde44c3c5c6
Hajime Nakagami <nakagami@gmail.com>
2011-06-13 17:00:38 +0900

    default database file path name revert.

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/dotcloud/planet.db',
+        'NAME': os.path.join(os.getcwd(), 'planet.db'),
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
