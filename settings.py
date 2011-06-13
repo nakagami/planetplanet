ca2d2704358ecb6c863c248f1626b9bc5a565d0b
Hajime Nakagami <nakagami@gmail.com>
2011-06-13 18:01:19 +0900

    media path adjust

--- settings.py
+++ settings.py
@@ -46,7 +46,7 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/dotcloud/current/planetplanet/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/static/media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'z1tzvrc#*9vhu0y#-^=_wmalpib@2e6_ac6(tjph6qe0gz)$2m'
