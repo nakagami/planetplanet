84545095ac3daf339fc94ddb94dafbdfaa2f70bd
Hajime Nakagami <nakagami@gmail.com>
2021-10-19 13:05:14 +0900

    modify urlpatterns

--- planetplanet/urls.py
+++ planetplanet/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import include, path
 
-urlpatterns = patterns('',
+urlpatterns = [
     path('^$', lambda request: redirect('^planet/', permanent=False)),
-    path(r'^planet/', include('planetplanet.planet.urls')),
+    path(r'^planet/', include('planet.urls')),
     path(r'^admin/', admin.site.urls),
-)
+]
