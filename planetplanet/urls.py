5e613b3f1e3a138ba3f5d757133682e9ba52410f
Hajime Nakagami <nakagami@gmail.com>
2021-10-19 12:37:06 +0900

    modify urls.py

--- planetplanet/urls.py
+++ planetplanet/urls.py
@@ -1,21 +1,9 @@
-"""planetplanet URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.shortcuts import redirect
+from django.urls import include, path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = patterns('',
+    path('^$', lambda request: redirect('^planet/', permanent=False)),
+    path(r'^planet/', include('planetplanet.planet.urls')),
+    path(r'^admin/', admin.site.urls),
+)
