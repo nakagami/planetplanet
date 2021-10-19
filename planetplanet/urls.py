b9304362bb719bc965e0f8e63fa061a435bb883c
Hajime Nakagami <nakagami@gmail.com>
2021-10-19 16:33:05 +0900

    fix redirect URL

--- planetplanet/urls.py
+++ planetplanet/urls.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 from django.urls import include, re_path
 
 urlpatterns = [
-    re_path('^$', lambda request: redirect('^planet/', permanent=False)),
+    re_path('^$', lambda request: redirect('planet/', permanent=False)),
     re_path(r'^planet/', include('planet.urls')),
     re_path(r'^admin/', admin.site.urls),
 ]
