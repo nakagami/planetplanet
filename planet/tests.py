f802f9aeef2eeb91ebf50b37337381249062c552
Hajime Nakagami <nakagami@gmail.com>
2021-10-23 18:12:44 +0900

    add view test

--- planet/tests.py
+++ planet/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class PlanetViewTest(TestCase):
+    def test_index_template(self):
+        response = self.client.get("/planet/")
+        self.assertTemplateUsed(response, "planet/index.html")
